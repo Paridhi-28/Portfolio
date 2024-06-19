342281791116402c17f928abbac7e214594ef2a0
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-06-19 14:36:27 +0530

    Set up GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // other configurations can go here
-    basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
-  };
-  
-  import dotenv from 'dotenv';
-  dotenv.config();
-  
-  export default {
+    // other configurations
+};
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
     ...nextConfig,
     env: {
-      GMAIL_USER: process.env.GMAIL_USER,
-      GMAIL_PASS: process.env.GMAIL_PASS,
+        GMAIL_USER: process.env.GMAIL_USER,
+        GMAIL_PASS: process.env.GMAIL_PASS,
     },
-  };
-  
+};
