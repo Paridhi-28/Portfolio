2c9fd517cbd68220ab10e518f9b07d5b1746bf83
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-06-19 14:45:23 +0530

    Set up GitHub Pages deployment

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -48,9 +48,9 @@ const EmailSection = () => {
         <section className='grid md:grid-cols-2 my-12 py-24 gap-4 relative'>
             <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#D13ABD] to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2'>..</div>
             <div className='z-10'>
-                <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
+                <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
                 <p className='text-[#ADB7BE] mb-4 max-w-md'>
-                    I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you !!!
+                    I am currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I will try my best to get back to you !!!
                 </p>
                 <div className='socials flex flex-row gap-2'>
                     <Link href="https://github.com/Paridhi-28">
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
                     transition={{ duration: 0.5 }}
                     className='col-span-8 place-self-center text-center sm:text-left justify-self-start' >
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#EEBD89] to-[#D13ABD]'>Hello, I&apos;m{" "}</span>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#EEBD89] to-[#D13ABD]'>Hello, I am{" "}</span>
                         <br></br>
 
                         <TypeAnimation
